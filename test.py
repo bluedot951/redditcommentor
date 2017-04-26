0f29d3aa030fc33f7683d5045f8c14b69e6f5f87
Chirag Bharadwaj <cb625@cornell.edu>
2017-04-25 21:45:00 -0400

    Created quiet Makefile for clean-up

--- test.py
+++ test.py
@@ -7,13 +7,19 @@ from generate import *
 from pipetools import pipe
 from pipetools.utils import foreach
 
-# Simple testing area.
+# Number of comments to generate.
+NUM_COMMENTS = 10
+
+# Testing arena for the n-gram comment generation.
 def main(argv):
+  # Start the timer.
   start = time.time()
 
-  files = ["data/2005-12.txt"]
+  # The list of files to learn the model on.
+  files = ["data/2005-12.txt","data/2006-05.txt"]
 
-  makeComment = (pipe
+  # OCaml-style composition to create a function from files to a reverse-map.
+  makeMap = (pipe
     | getData
     | convertToUTF
     | toComment
@@ -21,18 +27,23 @@ def main(argv):
     | list
     | getBigrams
     | getRevBiMap
-    | makeBiComment
   )
 
-  body = makeComment(files)
-  comment = Comment(body)
+  # Only create the map once to avoid repeated computation.
+  revMap = makeMap(files)
 
-  print(comment)
+  # Generate NUM_COMMENTS comments.
+  for i in range(NUM_COMMENTS):
+    body = makeBiComment(revMap)
+    comment = Comment(body)
+    print(comment)
 
+  # End the timer.
   end = time.time()
   totTime = end - start
 
-  if totTime >= 1.0:
+  # Give the running time in sec or msec, whichever is more readable.
+  if totTime >= 0.1:
     print "Running time: %s s" % totTime
   else:
     print "Running time: %s ms" % (totTime * 1000.)
