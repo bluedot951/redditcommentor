f5955f41770d205d22b6e30532a5236afcb1bfc8
Chirag Bharadwaj <cb625@cornell.edu>
2017-05-02 21:23:01 -0400

    Fixed data-renaming script

--- rename.sh
+++ rename.sh
@@ -6,7 +6,7 @@ for file in $DIR/*.bz2
 do
   bzip2 -d $file
   FILE=`echo "$file" | rev | cut -d'/' -f1 | rev | cut -d'.' -f1`
-  if [[ $FILE == 20* ]]
+  if [[ $FILE == RC_20* ]]
   then
     NEWFILE=`echo "$FILE" | cut -d'_' -f2`
     mv $DIR/$FILE $DIR/$NEWFILE.txt
